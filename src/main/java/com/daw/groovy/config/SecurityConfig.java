35125e6bf332602b5bbae27badc8e23745b912a3
Samuel Aguiar <72396666+aguiarsc@users.noreply.github.com>
2025-05-23 12:07:08 +0200

    Project uploaded

--- src/main/java/com/daw/groovy/config/SecurityConfig.java
+++ src/main/java/com/daw/groovy/config/SecurityConfig.java
@@ -85,10 +85,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
-            frontendUrl,
-            "http://localhost:5173",
-            "http://localhost",
-            "http://frontend"
+            frontendUrl
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
